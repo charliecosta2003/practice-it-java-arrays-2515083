c9bafdf4e19971c29825b29be5ccd8c9ee35ec44
Charlie Costa <87437017+charliecosta2003@users.noreply.github.com>
2024-01-20 04:55:56 +0000

    Implement Custom Array List

--- src/CustomArrayList.java
+++ src/CustomArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CustomArrayList<D> {
   
   private int size = 0;
@@ -9,15 +11,27 @@ public class CustomArrayList<D> {
   }
 
   public D get(int i) {
-      return null;
+    if (i >= size || i < 0) {
+      throw new IndexOutOfBoundsException("Index " + i + " Size " + size);
+    }
+    return (D) elements[i];
   }
 
   public void add(D item) {
-
+    if (size == elements.length) {
+      increaseCapacity();
+    }
+    elements[size] = item;
+    size++;
   }
 
   public int size() {
       return size;
+  }
+
+  private void increaseCapacity() {
+    int newSize = elements.length * 2;
+    elements = Arrays.copyOf(elements, newSize);
   }
 
 }
